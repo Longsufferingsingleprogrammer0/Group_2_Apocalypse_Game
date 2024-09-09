874e0aa0f6bfbbcfaa9ae2674b1d991edd21397b
redcactus5 <jmajor@wpi.edu>
2024-09-09 00:00:24 -0400

    more work

--- Apocalypse_Game/Assets/scripts/logo_script.cs
+++ Apocalypse_Game/Assets/scripts/logo_script.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class logo_script : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private float elsapsed;
+    [SerializeField] private int waitTimeSeconds;
+    [SerializeField] private string SceneToSwitchTo;
+
     void Start()
     {
         
@@ -14,5 +20,19 @@ public class logo_script : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        elsapsed += Time.deltaTime;
+
+        if (elsapsed >= ((float)waitTimeSeconds))
+        {
+            SceneManager.LoadScene(sceneName: SceneToSwitchTo);
+        }else if (Input.anyKey)
+        {
+            SceneManager.LoadScene(sceneName: SceneToSwitchTo);
+        }
+
     }
 }
