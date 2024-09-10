f7ff1a1f04eb1c1902861403365feeda57dc244f
redcactus5 <jmajor@wpi.edu>
2024-09-10 16:27:34 -0400

    create a reusable script for setpeices

---
+++ Apocalypse_Game/Assets/scripts/setpeice_scripts/Furnature_Setpeice.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Furnature_Setpeice : MonoBehaviour
+{
+    #region variables
+
+    //an array of all the texures used by the sprite
+    [SerializeField] private Sprite[] textures;
+
+    //the current texture index
+    private int currentTextureIndex;
+
+    //the type of setpeice
+    [SerializeField] private string setpeiceType;
+
+    //get our sprite renderer
+    private SpriteRenderer spriteRenderer;
+    #endregion
+
+
+
+
+
+
+
+
+    public void setRandomTexture()
+    {
+        setTexture(Random.Range(0, textures.Length));
+    }
+
+
+
+
+    public int getTextureCount()
+    {
+        return textures.Length;
+    }
+
+
+
+
+    public void setTexture(int textureIndex)
+    {
+        if ((textureIndex >= 0) && (textureIndex < textures.Length))
+        {
+            currentTextureIndex = textureIndex;
+        }
+        else
+        {
+            throw new System.Exception("invalid texture index for a " + setpeiceType + ". acceptable values are between 0 and " + textures.Length.ToString() + ". argument value was " + textureIndex.ToString());
+        }
+
+        spriteRenderer.sprite = textures[textureIndex];
+
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get our sprite renderer
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        //generate a texture index to use
+        currentTextureIndex = Random.Range(0, textures.Length);
+
+        //set our sprite to said index
+        spriteRenderer.sprite = textures[currentTextureIndex];
+
+
+    }
+
+}
