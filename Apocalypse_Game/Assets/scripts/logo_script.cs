6d607586605715acd7b9c75640f45c0cba536771
redcactus5 <jmajor@wpi.edu>
2024-09-10 13:16:31 -0400

    update logo screen

--- Apocalypse_Game/Assets/scripts/logo_script.cs
+++ Apocalypse_Game/Assets/scripts/logo_script.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,32 +9,137 @@ public class logo_script : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    #region timerVariables
+    //how long we have waited
     private float elsapsed;
-    [SerializeField] private int waitTimeSeconds;
+    //how long to wait
+    private float timer;
+    #endregion
+
+    //if the user skips the animation
+    private bool skip;
+
+    //what stage of the animation we are on
+    private int stage;
+
+    //just premaking a variable for later
+    private float newAlpha;
+
+    //sprite renderer and logo sprite
+    private GameObject logo;
+    private SpriteRenderer logoRenderer;
+
+    #region controlVariables
+    [SerializeField] private string whatActorTagToFade;
+    [SerializeField] private float fadeInTimeSeconds;
+    [SerializeField] private float holdTimeSeconds;
+    [SerializeField] private float fadeOutTimeSeconds;
     [SerializeField] private string SceneToSwitchTo;
+    #endregion
+
 
     void Start()
     {
+        //initialize our variables and start the fade in process
+        timer = fadeInTimeSeconds;
+        elsapsed = 0f;
+        stage = 0;
+        skip= true;
+        newAlpha = 0f;
+
+
+        //get the logo
+        logo = GameObject.FindWithTag(whatActorTagToFade);
+        if( logo == null)
+        {
+            Debug.Log("null logo");
+        }
         
+        logoRenderer = logo.GetComponent<SpriteRenderer>();
+        if( logoRenderer == null)
+        {
+            Debug.Log("null renderer");
+        }
+        logoRenderer.color = new Color(logoRenderer.color.r, logoRenderer.color.g, logoRenderer.color.b, 0f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 
     private void FixedUpdate()
     {
-        elsapsed += Time.deltaTime;
+        //keep track of the time
+        elsapsed += Time.fixedDeltaTime;
 
-        if (elsapsed >= ((float)waitTimeSeconds))
+        //if we hit our timer
+        if (elsapsed >= timer)
         {
-            SceneManager.LoadScene(sceneName: SceneToSwitchTo);
-        }else if (Input.anyKey)
-        {
-            SceneManager.LoadScene(sceneName: SceneToSwitchTo);
+            //reset and next stage
+            elsapsed -= timer;
+
+            stage++;
+            switch (stage)
+            {
+                case 1:
+                    //just finished fade in, switch to wait
+                    logoRenderer.color = new Color(logoRenderer.color.r, logoRenderer.color.g, logoRenderer.color.b, 1f);
+                    timer = holdTimeSeconds; 
+                    break;
+
+                case 2:
+                    //just finished wait, switch to fade out
+                    skip = true;
+                    timer = fadeOutTimeSeconds;
+                    break;
+
+                default:
+                    //change to next scene
+                    logoRenderer.color = new Color(logoRenderer.color.r, logoRenderer.color.g, logoRenderer.color.b, 0f);
+                    SceneManager.LoadScene(SceneToSwitchTo);
+                    break;
+            }
+            
         }
+        else
+        {
+            //if no stage transiton, perform the stage animation
+            switch (stage)
+            {
+                case 0:
+                    //fade in
+                    //update the logorenderer to a new alpha based on the current period
+                    newAlpha = Mathf.Lerp(0f, 1f, elsapsed / timer);
+                    logoRenderer.color = new Color(logoRenderer.color.r, logoRenderer.color.g, logoRenderer.color.b, newAlpha);
+                    break;
+
+                case 1:
+                    break;
+
+                case 2:
+                    newAlpha = Mathf.Lerp(1f, 0f, elsapsed / timer);
+                    logoRenderer.color = new Color(logoRenderer.color.r, logoRenderer.color.g, logoRenderer.color.b, newAlpha);
+                    break;
+
+                default:
+                    Debug.Log("unintended switch state error");
+                    break;
+
+            }
+
+
+            
+        }
+
+        //allow the user to skip directly to fade out
+        if (skip && Input.anyKeyDown)
+        {
+            skip = false;
+            
+            logoRenderer.color = new Color(logoRenderer.color.r, logoRenderer.color.g, logoRenderer.color.b, 1f);
+            stage = 2;
+            elsapsed = 0f;
+            timer = fadeOutTimeSeconds;
+        }
+
 
     }
 }
