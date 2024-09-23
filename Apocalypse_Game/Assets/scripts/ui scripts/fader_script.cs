d84fb45213e10f486b344eb8be595079fe941055
redcactus5 <jmajor@wpi.edu>
2024-09-23 12:14:19 -0400

    finish title screen for now

---
+++ Apocalypse_Game/Assets/scripts/ui scripts/fader_script.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+
+
+public class fader_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
